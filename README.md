7cfa0502a4682fd8596a96b2afbccb12a3090afc
amresh.kumar <amresh.kumar@petronas.com>
2023-10-13 18:00:34 +0530

    dockerization and seperated conatiner

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # BLOG
 
+Dockerizing a React application with Node.js Postgres and NginX - dev and prod 
+Dockerizing a React application with Node.js Postgres and NginX - dev and prod 
 
+You can run it in development mode: docker-compose up --build
+It contains Dockerfiles for client, server which you should push to your docker hub to be able
+to pull them down
 
 ![image](https://github.com/amresh474/BLOG/assets/57634167/49e06a3f-17b6-4f47-8406-13cc2ad879c1)
--- api/db.js
+++ api/db.js
@@ -1,4 +1,4 @@
-import { createPool } from 'mysql2/promise';
+/* import { createPool } from 'mysql2/promise';
 
 const config = {
     host: process.env.HOST,
@@ -8,4 +8,23 @@ const config = {
 }
 
 const pool = createPool(config);
-export const connection = await pool.getConnection();
+export const connection = await pool.getConnection(); */
+
+
+// Postgres client setup
+// const { Pool } = require("pg");
+import pkg from 'pg';
+const {Pool} = pkg;
+const pgClient = new Pool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+});
+
+pgClient.on("connect", client => {
+  client
+    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
+    .catch(err => console.log("PG ERROR", err));
+});
+export const connection = pgClient;
